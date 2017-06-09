244a4f2908e2af09d55fc06d6868b1c14a3d75b8
Richard Garrick <richg341@gmail.com>
2017-06-08 21:42:09 -0700

    modify forms, JS, CSS, HTML and add some img

---
+++ README.md
@@ -0,0 +1,27 @@
+# _{Richard Garrick Portfolio}_
+
+#### _{Vacation Destination Survey}, {06/8/2017}_
+
+#### By _**{Richard Garrick}**_
+
+## Description
+
+_{This is a webpage for Richard Garrick's Vacation Planner page.  It is constantly in a state of flux.}_
+
+## Setup/Installation Requirements
+
+* _There are no real setup instructions, it is a webpage using CSS/HTML/Bootstrap/JavaScript/Atom/Jquery_
+
+## Known Bugs
+
+_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+
+## Support and contact details
+
+_{The web site for this project is located at: https://rg-epicodus.github.io/ }_
+
+### License
+
+*{This content is provided using the MIT license}*
+
+Copyright (c) 2017 **_Richard Garrick_**
--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $("#mainPageSubmit").click(function(event) {
     $("#mainPage").addClass("hidden");
     $("#persInfo").removeClass("hidden");
-
     event.preventDefault();
   });
 
@@ -44,7 +43,30 @@ $(document).ready(function() {
   $("#foodWineSubmit").click(function(event) {
     $("#foodWine").addClass("hidden");
     $("#destination").removeClass("hidden");
+    console.log('Success');
 
     event.preventDefault();
   });
+});
+
+$("#persInfo form").submit(function(event) {
+var nameInput = $("input#name").val();
+var streetInput = $("input#street").val();
+var cityInput = $("input#city").val();
+var stateInput = $("input#state").val();
+var zipInput = $("input#zip").val();
+var phoneInput = $("input#phone").val();
+var emailInput = $("input#email").val();
+
+$(".name").text(nameInput);
+$(".street").text(streetInput);
+$(".city").text(cityInput);
+$(".state").text(stateInput);
+$(".zip").text(zipInput);
+$(".phone").text(phoneInput);
+$(".email").text(emailInput);
+
+$("#destination").show();
+
+event.preventDefault();
 });
